850e305018dc5328bf959caaf1bd2802cf0181b0
Riscait <mr@nerco.jp>
2019-05-16 08:21:00 +0900

    Initial Commit

---
+++ FastIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastIssue
+//
+//  Created by 村松龍之介 on 2019/05/16.
+//  Copyright © 2019 村松龍之介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
