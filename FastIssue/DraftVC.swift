134ba963973a828aba0c78590d1e0673f5b335cf
Riscait <mr@nerco.jp>
2019-05-16 23:45:38 +0900

    DraftVCを追加

---
+++ FastIssue/DraftVC.swift
@@ -0,0 +1,32 @@
+//
+//  DraftVC.swift
+//  FastIssue
+//
+//  Created by 村松龍之介 on 2019/05/16.
+//  Copyright © 2019 村松龍之介. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class DraftVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
